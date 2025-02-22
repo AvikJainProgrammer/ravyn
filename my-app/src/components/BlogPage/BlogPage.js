9dac181b5bfc281c826b94bfc7b35a575199dff3
Avik Jain <avikjain7755331111@gmail.com>
2025-02-22 15:10:55 +0530

    changed scrollspy

--- my-app/src/components/BlogPage/BlogPage.js
+++ my-app/src/components/BlogPage/BlogPage.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import ScrollSpySidebar from '../../components/ScrollSpySidebar/ScrollSpySidebar';
-import ContentSection from '../../components/ContentSection/ContentSection';
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ScrollSpySidebar from "../../components/ScrollSpySidebar/ScrollSpySidebar";
+import ContentSection from "../../components/ContentSection/ContentSection";
 
 const BlogPage = ({ sections }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    }
+  }, [location]);
+
   return (
     <Container fluid>
       <Row>
         {/* Left Sidebar with ScrollSpy Navigation */}
         <Col md={3}>
-          <ScrollSpySidebar 
-            sections={sections.map(({ id, label }) => ({ id, label }))}
-          />
+          <ScrollSpySidebar sections={sections} />
         </Col>
 
         {/* Main Content */}
--- my-app/src/components/ScrollSpySidebar/ScrollSpySidebar.js
+++ my-app/src/components/ScrollSpySidebar/ScrollSpySidebar.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import Scrollspy from 'react-scrollspy';
-import { Nav } from 'react-bootstrap';
-import './ScrollSpySidebar.css';
+import React from "react";
+import Scrollspy from "react-scrollspy";
+import { Nav } from "react-bootstrap";
+import "./ScrollSpySidebar.css";
 
 const ScrollSpySidebar = ({ sections }) => {
-  // Extract the IDs from the sections
   const sectionIds = sections.map((section) => section.id);
 
   return (
@@ -12,7 +11,19 @@ const ScrollSpySidebar = ({ sections }) => {
       <Scrollspy items={sectionIds} currentClassName="active" offset={-100}>
         {sections.map((section) => (
           <Nav.Item key={section.id}>
-            <Nav.Link href={`#${section.id}`}>{section.label}</Nav.Link>
+            <Nav.Link
+              href={`#${section.id}`}
+              onClick={(e) => {
+                e.preventDefault();
+                const element = document.getElementById(section.id);
+                if (element) {
+                  element.scrollIntoView({ behavior: "smooth", block: "start" });
+                  window.history.pushState(null, "", `#${section.id}`);
+                }
+              }}
+            >
+              {section.label}
+            </Nav.Link>
           </Nav.Item>
         ))}
       </Scrollspy>
